5a9f9db3b0c7b4c341c3c0cdc38605f0296e4d93
Jean Connory <michaelrhcp@gmail.com>
2023-01-31 23:06:15 -0300

    first commit

---
+++ Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using LiteDB;
+
+namespace AppControleFinanceiro.Models
+{
+	public class Transaction
+	{
+		[BsonId]
+		public int Id { get; set; }
+
+		public TransactionType Type { get; set; }
+
+		public string Name { get; set; }
+
+		public DateTimeOffset Date { get; set; } //DatetimeOffset independe do fuso horário
+
+		public decimal Value { get; set; }
+	}
+
+	public enum TransactionType
+	{
+		Income,
+		Expenses
+	}
+}
---
+++ Repositories/TransactionRepository.cs
@@ -0,0 +1,49 @@
+﻿using AppControleFinanceiro.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Repositories
+{
+	public class TransactionRepository : ITransactionRepository
+	{
+		private readonly LiteDatabase _database;
+		private readonly string collectionName = "transactions";
+
+		public TransactionRepository()
+		{
+			_database = new LiteDatabase("Filename=C:/users/AppData/database.db;Connection=Shared");
+		}
+
+		public List<Transaction> GetAll()
+		{
+			return _database
+				.GetCollection<Transaction>(collectionName)
+				.Query()
+				.OrderByDescending(a => a.Date)
+				.ToList();
+		}
+
+		public void Add(Transaction transaction)
+		{
+			var col = _database.GetCollection<Transaction>(collectionName);
+			col.Insert(transaction);
+			col.EnsureIndex(a => a.Date);
+		}
+
+		public void Update(Transaction transaction)
+		{
+			var col = _database.GetCollection<Transaction>(collectionName);
+			col.Update(transaction);
+		}
+
+		public void Delete(Transaction transaction)
+		{
+			var col = _database.GetCollection<Transaction>(collectionName);
+			col.Delete(transaction.Id);
+		}
+	}
+}
