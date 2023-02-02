8941f04762f82538df9ebe6495ea7e07dca7f1be
Jean Connory <michaelrhcp@gmail.com>
2023-02-02 20:37:16 -0300

    grid saldo and validations

--- Views/TransactionAdd.xaml.cs
+++ Views/TransactionAdd.xaml.cs
@@ -1,3 +1,7 @@
+
+
+using System.Text;
+
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionAdd : ContentPage
@@ -11,4 +15,40 @@ public partial class TransactionAdd : ContentPage
 	{
 		Navigation.PopModalAsync();
     }
+
+	private void OnButtonClicked_Save(object sender, EventArgs e)
+	{
+
+	}
+
+	private bool IsValidData()
+	{
+		bool valid = true;
+		StringBuilder sb = new StringBuilder();
+
+
+		if(string.IsNullOrEmpty(EntryName.Text) || string.IsNullOrWhiteSpace(EntryName.Text))
+		{
+			sb.AppendLine("O campo 'Nome' deve ser preenchido!");
+			valid = false;
+		}
+		if (string.IsNullOrEmpty(EntryValue.Text) || string.IsNullOrWhiteSpace(EntryValue.Text))
+		{
+			sb.AppendLine("O campo 'Valor' deve ser preenchido!");
+			valid = false;
+		}
+
+		if (string.IsNullOrEmpty(EntryValue.Text) && !double.TryParse(EntryValue.Text, out double result))
+		{
+			sb.AppendLine("O campo 'Valor' é inválido!");
+			valid = false;
+		}
+
+		if(valid == false)
+		{
+			LabelError.Text = sb.ToString();
+		}
+
+		return valid;
+	}
 }
